638834c722e3fa8f2b6fe0a6959d2c9f39054941
Xing Zhou <xzhou@pivotal.io>
2018-05-22 15:24:43 -0400

    Work on duplication katas

--- duplication-kata-js/src/Lesson21.js
+++ duplication-kata-js/src/Lesson21.js
@@ -1,45 +1,40 @@
 const Song = require("./Song.js");
 
 class Lesson21 extends Song.Song {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    singSong(style, names) {
-        switch (style) {
-            case 1 :
-                for (var i = 0; i < names.length; i++){
-                    var name = names[i];
-                    if (name.startsWith("L", 0)) {
-                        this.sing("Hip Hip Horray! For " + name);
-                    }
-                    else {
-                        this.sing("Hello " + name + ", it's nice to meet you.");
-                    }
-                }
-                break;
-            case 2 :
-                for (var i = 0; i < names.length; i++){
-                    var name = names[i];
-                    if (name.startsWith("am", 1)) {
-                        this.sing("Say yeah! Say yo! Say " + name);
-                    }
-                    else {
-                        this.sing("Hello " + name + ", it's nice to meet you.");
-                    }
-                }
-                break;
-            case 3 :
-                for (var i = 0; i < names.length; i++){
-                    var name = names[i];
-                    this.sing("Hello " + name + ", it's nice to meet you.");
-                }
-                break;
-        }
+  singSong(style, names) {
+    switch (style) {
+      case 1 :
+        this.singVerse(names, true, "L", 0, "Hip Hip Horray! For ");
+        break;
+
+      case 2 :
+        this.singVerse(names, true, "am", 1, "Say yeah! Say yo! Say ");
+        break;
+
+      case 3 :
+        this.singVerse(names, false);
+        break;
     }
+  }
+
+  singVerse(names, shouldSingLyric, searchString, position, lyrics) {
+    for (var i = 0; i < names.length; i++) {
+      var name = names[i];
+      if (shouldSingLyric && name.startsWith(searchString, position)) {
+        this.sing(lyrics + name);
+      }
+      else {
+        this.sing("Hello " + name + ", it's nice to meet you.");
+      }
+    }
+  }
 }
 
 
 module.exports = {
-    Lesson21
+  Lesson21
 }
--- duplication-kata-js/src/Lesson2Variable.js
+++ duplication-kata-js/src/Lesson2Variable.js
@@ -1,21 +1,24 @@
 const Song = require("./Song.js");
+
 class Lesson2Variable extends Song.Song {
-    constructor(){
-        super();
-    }
-    singBottlesOfBeer() {
-        this.sing("100 bottles of beer on the wall");
-        this.sing("100 bottles of beer");
-        this.sing("Take one down, pass it around");
-        this.sing("99 bottles of beer on the wall");
-        this.sing("99 bottles of beer on the wall");
-        this.sing("99 bottles of beer");
-        this.sing("Take one down, pass it around");
-        this.sing("98 bottles of beer on the wall");
-    }
+  constructor() {
+    super();
+  }
+
+  singBottlesOfBeer() {
+    this.singVerse(100);
+    this.singVerse(99);
+  }
+
+  singVerse(bottlesOfBeer) {
+    this.sing(bottlesOfBeer + " bottles of beer on the wall");
+    this.sing(bottlesOfBeer + " bottles of beer");
+    this.sing("Take one down, pass it around");
+    this.sing((bottlesOfBeer - 1) + " bottles of beer on the wall");
+  }
 }
 
 
 module.exports = {
-    Lesson2Variable
+  Lesson2Variable
 }
